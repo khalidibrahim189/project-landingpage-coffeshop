96e913ad6f886c340eda5cef92fa21f105c476ff
khalid ibrahim <khalid.jambi18@gmail.com>
2022-02-09 08:42:14 +0700

    project2

--- assets/js/data.js
+++ assets/js/data.js
@@ -2,7 +2,10 @@ var toggler = document.getElementById("icon-toggle");
 var barr = document.getElementById("bar");
 var overlayy = document.getElementById("overlay");
 var list = document.getElementById("list");
-var close = document.getElementById("icon-close")
+var closer = document.getElementById("icon-close");
+var mainn2 = document.getElementById("main2");
+var toggler2 = document.getElementById("toggle2");
+var iconShop = document.getElementById("icon-shop");
 
 //start 
 toggler.onclick = function () {
@@ -11,7 +14,15 @@ toggler.onclick = function () {
   list.style.display = "flex"
 }
 
-close.onclick = function(){
+toggler2.onclick = function () {
+  mainn2.style.display = "none"
+}
+
+iconShop.onclick = function(){
+    mainn2.style.display = "block"
+}
+
+closer.onclick = function(){
     barr.style.display = "none"
     overlayy.style.display = "none"
     list.style.display = "none"
